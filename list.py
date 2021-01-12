4673527017d5d74de11fc5590d183cd4a9e5395e
Sardendu pandey <pandey.sardendu00@gmail.com>
2021-01-12 18:03:18 +0530

    Accepting data  and data type to convert into

--- list.py
+++ list.py
@@ -1,4 +1,17 @@
-x = input()
-l = list(x)
+def into_list(x):
+	l = list(x)
+	return l
 
-print(l)
+def into_str(x):
+	s = str(x)
+	return s
+def into_tuple(x):
+	t = tuple(x)
+	return t
+
+x = input ("enter the data you want to convert into ::")
+form = input(" enter the data type to convert::")
+if form == 'str':
+	print(into_str(x))
+elif form == 'tuple' :
+	print(into_tuple(x))
